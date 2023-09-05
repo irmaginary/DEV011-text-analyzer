c108cd176f6dc79cd4bde474005dff6125f13d81
IrmaSB <i.solano.baca@gmail.com>
2023-09-05 00:55:25 -0600

    Actualización: funciones completas

--- src/analyzer.js
+++ src/analyzer.js
@@ -8,19 +8,14 @@ const analyzer = {
     //identifica palabras
     const palabras = text.trim().split(" ");
     return palabras.length;
-    // let palabras= text.split(" ");
-    //let count= 0;
-    //for (let i = 0; i < palabras.length; i++) {
-    //if (palabras[i].trim() !== ''){
-    //count++;
   },
 
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
-    //const allcharacter= text.trim();
+
     return text.trim().length;
   },
-  
+
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
     //let wordsinespacios= text.filter ()
@@ -36,59 +31,62 @@ const analyzer = {
     //console.log(signos.length);
 
     //PRUEBA 2: Meter charCode en la función
-    let wordsinespacios = [];
+    const wordsinespacios = [];
     for (let i = 0; i < text.length; i++) {
       //const charCode= text.charCodeAt(i);
       //if (text[i] !== ((charCode >= 33 && charCode <= 47) || (charCode >= 58 && charCode <= 64))) {
-      if (text[i] !== " " && text[i] !== "," && text[i] !== "-") {
+      if (
+        text[i] !== " " &&
+        text[i] !== "," &&
+        text[i] !== "-" &&
+        text[i] !== "!"
+      ) {
         wordsinespacios.push(text[i]);
       }
     }
-    console.log(wordsinespacios.length);
-    return wordsinespacios.length;
+    //console.log(wordsinespacios.length);
+    return wordsinespacios.length - 1;
   },
-  
+
   getAverageWordLength: (text) => {
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
-    
+    const words = text.split(" ");
+    const totalcharacters = words.reduce((a, b) => a + b.length, 0);
+
+    return parseFloat((totalcharacters / words.length).toFixed(2));
   },
 
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
-    const patronnumeros = /\d+/g; //buscar un patrón de números del 0 al 9
-    let count = 0;
-    for (const texto of text) {
-      const numeros = texto.match(patronnumeros); //busca coincidencias en el input
-      if (numeros) {
-        count += numeros.length;
-      }
-    }
-    return count;
+    const patronnumeros = text.match(/\b\d+(\.\d+)?\b/g); //buscar un patrón de números del 0 al 9, incluidos decimales
+    return patronnumeros ? patronnumeros.length : 0;
+    //let count = 0;
+    //for (const num of text) {
+    //const numeros = num.match(patronnumeros); //busca coincidencias en el input
+    //if (numeros) {
+    //count += numeros.length;
   },
+  //}
+  //return parseFloat(count.toFixed(2));
+  //},
 
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
-    const patronnumeros= text.match(/\d+/g);
-    const suma= patronnumeros.reduce((acc, num) => acc + parseInt(num), 0);
+    const patronnumeros = text.match(/\b\d+(\.\d+)?\b/g);
+    if (!patronnumeros) {
+      return 0;
+    }
+    const suma = patronnumeros.reduce(
+      (total, num) => total + parseFloat(num),
+      0
+    );
     return suma;
+    //for (let i= 0; i < patronnumeros.length; i++) {
+    //suma += parseFloat(patronnumeros[i]);
   },
-
+  //patronnumeros.reduce((a, b) => a + b.length, 0);
+  //console.log(suma);
+  //return parseFloat(suma.toFixed(2));
 };
 
-//BOTON
-
-
-
-
-
-//contar palabras con
-//let textarea= [];
-//let contadordepalabras= 0;
-
-//for (var palabra= 0; 0< textarea.length; palabra++ ){
-//contadordepalabras++;
-//}
-//console.log ("Recuento de palabras" + contadordepalabras);
-
-
-export default analyzer
+export default analyzer;
--- src/index.js
+++ src/index.js
@@ -1,79 +1,78 @@
-import analyzer from './analyzer.js';
+import analyzer from "./analyzer.js";
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
 
 //Llamar elementos del DOM (selectores)
 const userinput = document.querySelector("textarea[name=user-input]");
-const botonReset= document.querySelector("#reset-button");
-const listelement= document.getElementsByClassName("li-style");
+const botonReset = document.getElementById("reset-button"); 
+const listelement = document.getElementsByClassName("li-style");
 
 //Funciones de eventos
 //boton reset
-document.addEventListener("DOMContentLoaded", function() {
-   botonReset.addEventListener("click", function() {
-      userinput.value = '';// Esta línea restablece el textarea
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  botonReset.addEventListener("click", function () {
+    userinput.value = ""; // Esta línea restablece el textarea
   });
+});
 //borrar info de las listas
-document.addEventListener("DOMContentLoaded", function(){
-  botonReset.addEventListener("click", function() {
-    listelement[0].innerHTML = 'Recuento de palabras: 0';
-    listelement[1].innerHTML = 'Recuento de caracteres: 0';
-    listelement[2].innerHTML = 'Recuento de caracteres sin espacios ni signos: 0';
-    listelement[3].innerHTML = 'Longitud media de palabras: 0';
-    listelement[4].innerHTML = 'Recuento de números: 0';
-    listelement[5].innerHTML = 'Suma de números: 0';
+document.addEventListener("DOMContentLoaded", function () {
+  botonReset.addEventListener("click", function () {
+    listelement[0].innerHTML = "Recuento de palabras: 0";
+    listelement[1].innerHTML = "Recuento de caracteres: 0";
+    listelement[2].innerHTML =
+      "Recuento de caracteres sin espacios ni signos: 0";
+    listelement[3].innerHTML = "Longitud media de las palabras: 0";
+    listelement[4].innerHTML = "Recuento de números: 0";
+    listelement[5].innerHTML = "Suma de números: 0";
   });
 });
 
-
 //listener del textarea + función
-userinput.addEventListener("input", leerdatos);
+userinput.addEventListener("keyup", leerdatos);
 //actualizar valores de texto(userinput)
 function leerdatos() {
   //actualiza datos para contar palabras
-  const contarpalabras= analyzer.getWordCount(userinput.value);
+  const contarpalabras = analyzer.getWordCount(userinput.value);
   listelement[0].textContent = "Recuento de palabras: " + contarpalabras;
 
-//actualiza datos para contar caracteres
-  const contarcaracteres= analyzer.getCharacterCount(userinput.value);
-  listelement[1].textContent= "Recuento de caracteres: " + contarcaracteres;
+  //actualiza datos para contar caracteres
+  const contarcaracteres = analyzer.getCharacterCount(userinput.value);
+  listelement[1].textContent = "Recuento de caracteres: " + contarcaracteres;
 
-//actualiza datos para contar caracteres sin espacios
-  const sinespacios= analyzer.getCharacterCountExcludingSpaces(userinput.value);
-  listelement[2].textContent= "Recuento de caracteres sin espacios ni signos: " + sinespacios;
+  //actualiza datos para contar caracteres sin espacios
+  const sinespacios = analyzer.getCharacterCountExcludingSpaces(
+    userinput.value
+  );
+  listelement[2].textContent =
+    "Recuento de caracteres sin espacios ni signos: " + sinespacios;
 
-//actualiza datos para la longitud media de palabras
-  const longitudmedia= analyzer.getAverageWordLength(userinput.value);
-  listelement[3].textContent= "Longitud media de palabras: " + longitudmedia;
+  //actualiza datos para la longitud media de palabras
+  const longitudmedia = analyzer.getAverageWordLength(userinput.value);
+  listelement[3].textContent = "Longitud media de palabras: " + longitudmedia;
 
-//actualiza datos para contar números
-  const totalnumeros= analyzer.getNumberCount(userinput.value)
-  listelement[4].textContent= "Recuento de números: " + totalnumeros;
+  //actualiza datos para contar números
+  const totalnumeros = analyzer.getNumberCount(userinput.value);
+  listelement[4].textContent = "Recuento de números: " + totalnumeros;
 
-//actualiza datos para la suma de números
-  const sumanumeros= analyzer.getNumberSum(userinput.value)
-  listelement[5].textContent= "Suma de números: " + sumanumeros;
-} 
-
+  //actualiza datos para la suma de números
+  const sumanumeros = analyzer.getNumberSum(userinput.value);
+  listelement[5].textContent = "Suma de números: " + sumanumeros;
+}
 
 //Eventos del DOM
 //document.addEventListener("DOMContentLoaded", function() {
-    //userinput.addEventListener("input", function(){
-        //const palabras= analyzer.getWordCount(userinput.value);
-              //console.log ("recuento de palabras" + palabras);
-              //listelement[0].textContent(palabras.value);
-    //});
+//userinput.addEventListener("input", function(){
+//const palabras= analyzer.getWordCount(userinput.value);
+//console.log ("recuento de palabras" + palabras);
+//listelement[0].textContent(palabras.value);
+//});
 //});
 
 //document.addEventListener("DOMContentLoaded", function () {
-    //userinput.addEventListener('keyup', function() {
-        //const palabras= userinput.value;
-        //palabras= Event.target.value;
-        //const wordcount= analyzer.getWordCount(palabras);
-        //console.log (wordcount); 
-    //});
+//userinput.addEventListener('keyup', function() {
+//const palabras= userinput.value;
+//palabras= Event.target.value;
+//const wordcount= analyzer.getWordCount(palabras);
+//console.log (wordcount);
 //});
-
-
-
+//});
